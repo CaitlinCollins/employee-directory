d6e445c52c6e74ff548ea545c1f590ec0e2c00e1
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-04-27 15:09:27 -0500

    Created Header component

--- build/src/App.js
+++ build/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-import Search from "./components/Search";
-import Employee from "./components/Employee";
+// import Search from "./components/Search";
+// import Employee from "./components/Employee";
 
 function App() {
     return (
         <div className="container">
             <Header />
-            <Search />
-            <Employee />
+         
         </div>
     )
 }
---
+++ build/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h1>Employee Directory</h1>
+            <p>Click on carrots to filter heading or use the search box to narrow your results.</p>
+        </header>
+    )
+}
+
+export default Header;
