67416167f897da9444a58cd87dc7ecbc8b7842c6
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-04-27 15:46:21 -0500

    Created Api.js

--- build/src/App.js
+++ build/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
-// import Search from "./components/Search";
+import Search from "./components/Search";
 // import Employee from "./components/Employee";
+import "./App.css";
 
 function App() {
     return (
         <div className="container">
             <Header />
+            <Search />
          
         </div>
     )
--- build/src/components/Search.js
+++ build/src/components/Search.js
@@ -1,1 +1,15 @@
 import React from "react";
+import './Search.css';
+
+function Search() {
+    return (
+        <div className="search">
+            <form className="form-inline">
+                <input className="form-control" type="search" aria-label="Search"></input>
+                <button className="btn btn-outline-primary"type="submit">Search</button>
+            </form>
+        </div>
+    );
+}
+
+export default Search;
